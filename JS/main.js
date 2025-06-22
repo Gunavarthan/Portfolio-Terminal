300b015d4e80c4dd55b9ba55cfb2109c05bbe57e
Gunavarthan S <gunavarthan832@gmail.com>
2025-06-22 11:41:38 +0530

    hmm.

--- JS/main.js
+++ JS/main.js
@@ -1013,6 +1013,7 @@ async function ListProject()
     term.pause();
     CanAsk = false;
     Download("projects-List");
+    term.focus();
     scrollTerminalToBottom();
     await sleep(3000)
     CanAsk = true;
